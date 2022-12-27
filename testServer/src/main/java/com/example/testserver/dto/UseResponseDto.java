f552a05fcb940f953a30fe8282f52e066fe60c5f
faulty337 <faulty337@gmail.com>
2022-12-27 11:05:20 +0900

    Feat : 테스트 서버 회원 조회 및 토큰 확인 기능 추가

---
+++ testServer/src/main/java/com/example/testserver/dto/UseResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.testserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UseResponseDto {
+    private String username;
+    private String nickname;
+}
